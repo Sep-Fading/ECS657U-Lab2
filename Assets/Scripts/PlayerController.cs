0632d779e1011c0af2bb6241baa877cb6484d143
Sepehr <main.sepehr@gmail.com>
2024-10-14 17:07:21 +0100

    next part

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     public Vector2 movevalue;
     public float speed;
+
+    public int score = 0;
     // Start is called before the first frame update
     void OnMove(InputValue value)
     {
@@ -16,5 +18,18 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movevalue.x, 0.0f, movevalue.y);
         GetComponent<Rigidbody>().AddForce(movement*speed*Time.fixedDeltaTime);
+
+        if (score == 3)
+        {
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            score += 1;
+        }
     }
 }
