69527eb72a6e13db0005ac8cb17e5d10f8111e86
David Ghorbani-Nia <d.ghorbani-nia@se22.qmul.ac.uk>
2024-10-15 16:16:46 +0100

    added physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public Vector2 movevalue;
+    public Vector2 moveValue;
     public float speed;
     // Start is called before the first frame update
     void OnMove(InputValue value)
     {
-        movevalue = value.Get<Vector2>();
+        moveValue = value.Get<Vector2>();
     }
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movevalue.x, 0.0f, movevalue.y);
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
         GetComponent<Rigidbody>().AddForce(movement*speed*Time.fixedDeltaTime);
     }
 }
